0ebb8183fbd226b51ee25181132a4940fe02810b
peterparkerdata <peter.parker.data@gmail.com>
2017-12-12 15:40:10 -0500

    Updated exercise file(s)

--- peterparkerdata/exercise.js
+++ peterparkerdata/exercise.js
@@ -1,39 +1,31 @@
 var exercise = {};
 exercise.hobbies = [];
-// return list of people taking 1.00
-exercise.getCourseList = function(courseName) {
-    var list = [];
+exercise.getCourseList = function(name) {
+    var people = [];
     var dataLength = exercise.data.contacts.length;
     var target = exercise.data.contacts;
     var person;
-    // enter code to list each person that takes this course
     for (var i = 0; i < dataLength; i++) {
         person = target[i];
         if (person.courses.includes(name))
             people.push(person);
     }
-    
+
     return people;
 };
-
 var check = function(item) {
     if (!exercise.hobbies.includes(item)) {
         exercise.hobbies.push(item);
     }
 }
-
-// return list of hobbies
-exercise.getHobbies = function() {
+exercise.getHobbies = function(salary) {
     var dataLength = exercise.data.contacts.length;
     var target = exercise.data.contacts;
     var person;
-    // enter code to get person and their hobbies
-    // add new hobby to exercise.hobbies;
     for (var i = 0; i < dataLength; i++) {
         person = target[i];
         person.hobbies.forEach(check);
     }
-
     return exercise.hobbies;
 };
 
