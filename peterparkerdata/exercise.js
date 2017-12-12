659912b2255600dfa7ccbb32598d9af108f9e067
peterparkerdata <peter.parker.data@gmail.com>
2017-12-12 15:08:32 -0500

    Updated exercise file(s)

--- peterparkerdata/exercise.js
+++ peterparkerdata/exercise.js
@@ -7,8 +7,21 @@ exercise.getCourseList = function(courseName) {
     var people = exercise.data.contacts;
     var person;
     // enter code to list each person that takes this course
+    for (var i = 0; i < dataLength; i++) {
+        person = target[i];
+        if (person.courses.includes(name))
+            people.push(person);
+    }
+    
     return people;
 };
+
+var check = function(item) {
+    if (!exercise.hobbies.includes(item)) {
+        exercise.hobbies.push(item);
+    }
+}
+
 // return list of hobbies
 exercise.getHobbies = function() {
     var dataLength = exercise.data.contacts.length;
@@ -16,6 +29,14 @@ exercise.getHobbies = function() {
     var person;
     // enter code to get person and their hobbies
     // add new hobby to exercise.hobbies;
+    for (var i = 0; i < dataLength; i++) {
+        person = target[i];
+        person.hobbies.forEach(check);
+    }
 
     return exercise.hobbies;
 };
+
+exercise.test = function() {
+    return {"a": true};
+}
